1477f7046f05ac26bf65d06771bfa35860a0ab64
ChrisDoudesis <s242851@dtu.dk>
2026-06-03 16:46:31 +0200

    test

--- Parquet loading example.py
+++ Parquet loading example.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-# df = pd.read_parquet('house-price.parquet')
-# df.head()
-# df.info()
-# df.describe()
+df = pd.read_parquet('data/house-price.parquet')
+df.head()
+df.info()
+df.describe()
 
 # print(df.columns)
 
 # import os
 # print(os.getcwd())
-import os
-from pathlib import Path
+# import os
+# from pathlib import Path
 
-print("cwd:", os.getcwd())                 # folder the IDE runs from
-print("__file__:", Path(__file__).resolve())  # full path of this script
-print("script dir:", Path(__file__).resolve().parent)
+# print("cwd:", os.getcwd())                 # folder the IDE runs from
+# print("__file__:", Path(__file__).resolve())  # full path of this script
+# print("script dir:", Path(__file__).resolve().parent)
